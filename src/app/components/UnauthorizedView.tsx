1d5fa112a52018b412da348b2749a0f7c240ebac
GavinBoller <gavinboller@outlook.com>
2025-07-22 16:52:33 +1000

    rebrand

--- src/app/components/UnauthorizedView.tsx
+++ src/app/components/UnauthorizedView.tsx
@@ -6,7 +6,7 @@ import SignInButton from './SignInButton';
 export default function UnauthorizedView() {
   return (
     <div className="my-stations-container">
-      <h1 className="title">Wind App</h1>
+      <h1 className="title">Wind sniff</h1>
       <p>Please sign in to access your stations.</p>
       <SignInButton />
     </div>
